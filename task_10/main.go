ce7dcebc82499a1c6b3c6ce723a47d0016b99ec1
Maksim Torzhkov <maxtorj@yandex.ru>
2026-02-23 10:10:07 +0300

    feat: add divide function

---
+++ task_10/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+)
+
+func main() {
+	fmt.Println("Starting application...")
+	
+	result, err := divide(10, 0)
+	if err != nil {
+		log.Printf("Error: %v", err)
+	}
+	
+	log.Printf("Result: %d", result)
+}
+
+func divide(a, b int) (int, error) {
+	if b == 0 {
+		return 0, errors.New("division by zero")
+	}
+	return a / b, nil
+}
+
+// TODO: add more functions - сработает предупреждение без issue
+// TODO-123: implement multiplication - ок, есть issue
